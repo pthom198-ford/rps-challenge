6ce63190d590a0eca09c96e1ca4a6691a169c55e
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-31 14:32:12 +0100

    passed secon feature test to register name

--- rps_web.rb
+++ rps_web.rb
@@ -3,9 +3,15 @@ require 'sinatra/base'
 class RpsWeb < Sinatra::Base
 
 get '/' do
-  'up and running!'
+  erb :form
 end
 
+
+post '/registered' do
+  @user_name = params[:user_name]
+  p params
+  erb :register
+  end
 
   run! if app_file == $0
 end
--- spec/features/rps_web_spec.rb
+++ spec/features/rps_web_spec.rb
@@ -1,16 +1,16 @@
 
-feature 'Running web app' do
-  scenario 'can run app and check page content' do
-    visit('/')
-    expect(page).to have_content('up and running!')
-  end
-end
+# feature 'Running web app' do
+#   scenario 'can run app and check page content' do
+#     visit('/')
+#     expect(page).to have_content('up and running!')
+#   end
+# end
 
 feature 'Registering name' do
   scenario 'Can enter name and register' do
-    visit('/enter')
+    visit('/')
     fill_in 'Enter name', with: 'Piers'
-    click_button('Register')
+    click_button('Submit')
     expect(page).to have_content('Piers')
   end
 end
