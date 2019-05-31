6bc1fc7fad496cd0bac7b18c996a29aca52913e7
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-31 11:29:53 +0100

    add second test for register name

--- rps_web.rb
+++ rps_web.rb
@@ -2,5 +2,10 @@ require 'sinatra/base'
 
 class RpsWeb < Sinatra::Base
 
+get '/' do
+  'up and running!'
+end
+
+
   run! if app_file == $0
 end
--- spec/features/rps_web_spec.rb
+++ spec/features/rps_web_spec.rb
@@ -1,9 +1,16 @@
 
-feature 'Register name' do
+feature 'Running web app' do
   scenario 'can run app and check page content' do
     visit('/')
     expect(page).to have_content('up and running!')
-
   end
+end
 
+feature 'Registering name' do
+  scenario 'Can enter name and register' do
+    visit('/enter')
+    fill_in 'Enter name', with: 'Piers'
+    click_button('Register')
+    expect(page).to have_content('Piers')
+  end
 end
